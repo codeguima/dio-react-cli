be83455587542d699ba368ae3fd3b0b302c72f6a
Jhonny Guimarães <111026602+codeguima@users.noreply.github.com>
2024-12-21 22:29:31 -0300

    ok

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
+import { Home } from './pages/home';
+import { Login } from './pages/login';
 
 function App() {
   return (
-    <div className="App">
-      Olá DIO
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/login/index.jsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+    return(<>
+        <h1>Login</h1>
+        <Link to="/home">Voltar Home</Link>
+    </>)
+}
+
+export {Login}
