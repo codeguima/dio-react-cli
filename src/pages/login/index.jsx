a539a8fecfa2a592ffc0dfd248db9ee69af82db4
Jhonny Guimarães <111026602+codeguima@users.noreply.github.com>
2024-12-26 13:56:03 -0300

    ok

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,48 +1,80 @@
 import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
-import {MdEmail, MdLock} from 'react-icons/md';
+import { MdEmail, MdLock } from 'react-icons/md';
 import { Button } from '../../components/Button/index';
 import { Header } from '../../components/Header/index';
 import { Input } from '../../components/Input/index';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
 
-import {Column, Container, CriarText, EsqueciText, Row, SubtitleLogin, Title, TitleLogin, Wrapper } from './styles';
+import { Column, Container, CriarText, EsqueciText, Row, SubtitleLogin, Title, TitleLogin, Wrapper } from './styles';
 
+const schema = yup.object({
+    email: yup.string().email('E-mail não é válido').required('Campo Obrigatório'),
+    password: yup.string().min(3, 'No mínimo 3 caracteres').required('Campo Obrigatório'),
+}).required();
 
 const Login = () => {
-
     const navigate = useNavigate();
 
-    const handleClickSignIn = () =>{
-        navigate('/feed')
-    }
+    const { control, handleSubmit, formState: { errors, isValid } } = useForm({
+        resolver: yupResolver(schema),
+        mode: 'onChange',
+    });
 
-    return(<>
-        <Header />
-        <Container>
-            <Column>
-                <Title>
-                    A plataforma para você aprender com experts, dominar as principais
-                    tecnologias e entrar mais rápido nas empresas mais desejadas.
-                </Title>
-            </Column>
-            <Column>
-            <Wrapper>
-                <TitleLogin>Faça seu cadastro</TitleLogin>
-                <SubtitleLogin>Faça seu Login</SubtitleLogin>
-                <form>
-                    <Input placeholder="E-mail" leftIcon={<MdEmail/>}/>
-                    <Input placeholder="Senha" type="password" leftIcon={<MdLock/>}/>
-                    <Button title="Entrar" variant="secondary" onClick={handleClickSignIn} type="button"/>
-                </form>
-                <Row>
-                    <EsqueciText>Esqeci minha senha </EsqueciText>
-                    <CriarText>Criar conta</CriarText>
-                </Row>
-            </Wrapper>
-                
-            </Column>
-        </Container>
-    </>)
-}
+    console.log(isValid, errors);
 
-export {Login}
+    const onSubmit = data => {
+        console.log(data);
+        handleClickSignIn(); // Redirecionar após login bem-sucedido
+    };
+
+    const handleClickSignIn = () => {
+        navigate('/feed');
+    };
+
+    return (
+        <>
+            <Header />
+            <Container>
+                <Column>
+                    <Title>
+                        A plataforma para você aprender com experts, dominar as principais
+                        tecnologias e entrar mais rápido nas empresas mais desejadas.
+                    </Title>
+                </Column>
+                <Column>
+                    <Wrapper>
+                        <TitleLogin>Faça seu cadastro</TitleLogin>
+                        <SubtitleLogin>Faça seu Login</SubtitleLogin>
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <Input
+                                name="email"
+                                control={control}
+                                placeholder="E-mail"
+                                leftIcon={<MdEmail />}
+                                errorMessage={errors?.email?.message}
+                            />
+                            <Input
+                                name="password"
+                                control={control}
+                                placeholder="Senha"
+                                type="password"
+                                leftIcon={<MdLock />}
+                                errorMessage={errors?.password?.message} 
+                            />
+                            <Button title="Entrar" variant="secondary" type="submit" />
+                        </form>
+                        <Row>
+                            <EsqueciText>Esqueci minha senha</EsqueciText>
+                            <CriarText>Criar conta</CriarText>
+                        </Row>
+                    </Wrapper>
+                </Column>
+            </Container>
+        </>
+    );
+};
+
+export { Login };
